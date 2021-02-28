aa8d3dfdc74e9e1864fce0aa48e91c5dc877b11b
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-02-28 21:47:48 +0800

    #1

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ sudo apt install -y ansible
 
 #git clone --depth 1 https://git.linaro.org/ci/job/configs.git
 cd configs/ldcg-python-tensorflow/tensorflow
-ansible-playbook -i inventory playbooks/run.yml
+ansible-playbook -i inventory --verbose  playbooks/run.yml
