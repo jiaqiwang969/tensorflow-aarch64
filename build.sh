b7302b847347e13c86d6bd04cce0ba7743b44708
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-02-28 21:18:25 +0800

    #1

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ sudo apt update -y
 #apt upgrade -y
 sudo apt install -y ansible
 
-git clone --depth 1 https://git.linaro.org/ci/job/configs.git
+#git clone --depth 1 https://git.linaro.org/ci/job/configs.git
 cd configs/ldcg-python-tensorflow/tensorflow
 ansible-playbook -i inventory playbooks/run.yml
